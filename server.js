e995c515accac5650bf3267fb0cf65996b6047e2
Vaishnavi Jawalkar <jawalkarvaishnavi604@gmail.com>
2025-09-30 23:09:43 +0530

    started the server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "public/html", "index.html"));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
